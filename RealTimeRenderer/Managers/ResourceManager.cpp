b8882f1afa170a4da6045c23922b65e7a2ea1bf6
Matei Giurgiu <mateigiurgiu@gmail.com>
2019-01-03 23:23:17 +0000

    Fixed some issues and added texture loading

--- RealTimeRenderer/Managers/ResourceManager.cpp
+++ RealTimeRenderer/Managers/ResourceManager.cpp
@@ -1,10 +1,14 @@
 #include "pch.h"
 #include "ResourceManager.h"
+#include "Texture.h"
 #include "Mesh.h"
 #include "Shader.h"
 
 std::map<wchar_t*, std::shared_ptr<Mesh>> ResourceManager::m_meshes = {};
 std::map<wchar_t*, std::shared_ptr<Shader>> ResourceManager::m_shaders = {};
+std::map<std::wstring, std::shared_ptr<Texture>> ResourceManager::m_textures = {};
+
+int ResourceManager::MeshesLoaded = { 0 };
 
 std::shared_ptr<Mesh> ResourceManager::GetMesh(const wchar_t* filename, ID3D11Device1* device)
 {
@@ -16,6 +20,7 @@ std::shared_ptr<Mesh> ResourceManager::GetMesh(const wchar_t* filename, ID3D11De
 	else
 	{
 		m_meshes[const_cast<wchar_t*>(filename)] = std::make_shared<Mesh>(filename, device);
+		MeshesLoaded++;
 		return m_meshes[const_cast<wchar_t*>(filename)];
 	}
 }
@@ -23,7 +28,7 @@ std::shared_ptr<Mesh> ResourceManager::GetMesh(const wchar_t* filename, ID3D11De
 std::shared_ptr<Shader> ResourceManager::GetShader(const wchar_t* filename, ID3D11Device1* device)
 {
 	// check if this shader was already loaded
-	if (m_meshes.count(const_cast<wchar_t*>(filename)) == 1)
+	if (m_shaders.count(const_cast<wchar_t*>(filename)) == 1)
 	{
 		return m_shaders[const_cast<wchar_t*>(filename)];
 	}
@@ -33,3 +38,18 @@ std::shared_ptr<Shader> ResourceManager::GetShader(const wchar_t* filename, ID3D
 		return m_shaders[const_cast<wchar_t*>(filename)];
 	}
 }
+
+std::shared_ptr<Texture> ResourceManager::GetTexture(const std::wstring& filename, ID3D11Device1* device)
+{
+	// check if this texture was already loaded
+	if (m_textures.count(filename) == 1)
+	{
+		return m_textures[filename];
+	}
+	else
+	{
+		m_textures[filename] = std::make_shared<Texture>(device, filename);
+		return m_textures[filename];
+	}
+}
+
--- RealTimeRenderer/Managers/ResourceManager.h
+++ RealTimeRenderer/Managers/ResourceManager.h
@@ -11,9 +11,13 @@ public:
 
 	static std::shared_ptr<Mesh> GetMesh(const wchar_t* filename,  ID3D11Device1* device);
 	static std::shared_ptr<Shader> GetShader(const wchar_t* filename, ID3D11Device1* device);
+	static std::shared_ptr<Texture> GetTexture(const std::wstring& filename, ID3D11Device1* device);
+
+	static int MeshesLoaded;
 
 private:
 	static std::map<wchar_t*, std::shared_ptr<Mesh>> m_meshes;
 	static std::map<wchar_t*, std::shared_ptr<Shader>> m_shaders;
+	static std::map<std::wstring, std::shared_ptr<Texture>> m_textures;
 };
 
